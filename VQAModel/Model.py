7919ff3886532481565b1c668c84c4efb7820a68
JUKI <jyukipann@yahoo.co.jp>
2024-06-10 18:24:10 +0900

    vit追加

--- VQAModel/Model.py
+++ VQAModel/Model.py
@@ -1,7 +1,14 @@
 from keras.layers import Input
 from keras.models import Model
+from transformers import ViTImageProcessor, TFViTModel
 
 
-def VQAModel():
+def VQAModel(pretrained_vit_path='dump/vit-base-patch16-224-in21k'):
     visual_input = Input((3, 224, 224))
     text_token_input = Input((15, 512))
+    # processor = ViTImageProcessor.from_pretrained(pretrained_vit_path)
+    vit = TFViTModel.from_pretrained(pretrained_vit_path)
+    visual_embedding = vit(visual_input).last_hidden_state
+    # current shape is  (batch, 197, 768)
+    
+
